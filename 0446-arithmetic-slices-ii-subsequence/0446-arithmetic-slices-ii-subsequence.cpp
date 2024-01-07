0f418d8d15337602d0e1fcb9358ab14b97abba56
muqeetqazi <141923917+muqeetqazi@users.noreply.github.com>
2024-01-07 17:40:44 +0500

    Time: 400 ms (75.83%), Space: 109.9 MB (73.61%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.cpp
@@ -1,15 +1,29 @@
-class Solution:
-    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        n = len(nums)
-        total_count = 0  
-        dp = [defaultdict(int) for _ in range(n)]
+class Solution {
+public:
+    int numberOfArithmeticSlices(std::vector<int>& nums) {
+        int n = nums.size();
+        int total_count = 0;
 
-        for i in range(1, n):
-            for j in range(i):
-                diff = nums[i] - nums[j]
-                dp[i][diff] += 1  
-                if diff in dp[j]:
-                    dp[i][diff] += dp[j][diff]
-                    total_count += dp[j][diff]
+        std::vector<std::unordered_map<int, int>> dp(n);
 
-        return total_count
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                long long diff = static_cast<long long>(nums[i]) - nums[j]; 
+
+                if (diff > INT_MAX || diff < INT_MIN)
+                    continue; 
+
+                int diff_int = static_cast<int>(diff);
+
+                dp[i][diff_int] += 1; 
+
+                if (dp[j].count(diff_int)) {
+                    dp[i][diff_int] += dp[j][diff_int];
+                    total_count += dp[j][diff_int];
+                }
+            }
+        }
+
+        return total_count;
+    }
+};
