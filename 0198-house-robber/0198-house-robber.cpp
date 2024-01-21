45ec05b4cd759314bdadef0b9f3fe00a5f90718e
muqeetqazi <141923917+muqeetqazi@users.noreply.github.com>
2024-01-21 20:09:46 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+         int even=0;
+         int old=0;
+        
+        for(int i=0;i<nums.size();i=i+2)
+        {
+            if(i%2==0)
+            {
+                even=max(even+nums[1],old);
+            }
+            else{
+                old=max(old+nums[i],even);
+            }
+
+        }
+        return max(rob,newrob);
+    }
+};
