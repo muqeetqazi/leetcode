eae61f51c98f73527687a4ccd0ecc2fe5ab75e81
muqeetqazi <141923917+muqeetqazi@users.noreply.github.com>
2024-11-11 13:50:51 +0500

    Time: 0 ms (100%), Space: 9.6 MB (12.34%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,17 +1,30 @@
 class Solution {
-public:
-    int rob(vector<int>& nums) {
-        int even = 0;
-        int old = 0;
-        
-        for (int i = 0; i < nums.size(); i = i + 2) {
-            if (i % 2 == 0) {
-                even = max(even + nums[i], old);
-            } else {
-                old = max(old + nums[i], even);
-            }
-        }
 
-        return max(even, old);
+public:
+    int solve(vector<int>& nums,int n,vector<int>& dp)
+    {
+        if(n<0)
+        {
+            return 0;
+        }
+        if(n==0)
+        {
+            return nums[0];
+        }
+        if(dp[n]!=-1)
+        {
+            return dp[n];
+        }
+        int include=solve(nums,n-2,dp)+nums[n];
+        int exclude=solve(nums,n-1,dp)+0;
+        dp[n]= max(include,exclude);
+        return dp[n];
+    }
+
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+    vector<int> dp(n,-1);
+    
+    return solve(nums,n-1,dp);
     }
 };
