350cec278f1c2487bdef01448af0779ab8d704e3
muqeetqazi <141923917+muqeetqazi@users.noreply.github.com>
2024-11-20 17:57:30 +0500

    Time: 0 ms (100%), Space: 7.5 MB (78.9%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,48 +1,17 @@
 class Solution {
 public:
-    int solve(int n,vector<int>& dp)
-    {
-          if (n == 0) return 0;
-        if (n == 1) return 1;
-        if (dp[n] != -1) return dp[n];
-
-        int maxProduct = 0;
-        for(int i=1;i<n;++i)
-        {
-            maxProduct = max(maxProduct, max(i * (n - i), i * solve(n - i, dp)));
-        }
-        return dp[n]=maxProduct;
- //        int maxProduct = 0;
-//         for (int i = 1; i < n; ++i) {
-            
-//             maxProduct = max(maxProduct, max(i * (n - i), i * solve(n - i, dp)));
-//         }
-//         return dp[n] = maxProduct;
-
-    }
     int integerBreak(int n) {
-        vector<int>dp (n+1,-1);
-        return solve(n,dp);
-        
+        if(n<=3)
+        {
+            return n-1;
+        }
+        int product=1;
+        while(n>4)
+        {
+            product*=3;
+            n-=3;
+        }
+        product*=n;
+        return product;
     }
 };
-// class Solution {
-// public:
-//     int solve(int n, vector<int>& dp) {
-//         if (n == 0) return 0;
-//         if (n == 1) return 1;
-//         if (dp[n] != -1) return dp[n];
-
-//         int maxProduct = 0;
-//         for (int i = 1; i < n; ++i) {
-            
-//             maxProduct = max(maxProduct, max(i * (n - i), i * solve(n - i, dp)));
-//         }
-//         return dp[n] = maxProduct;
-//     }
-
-//     int integerBreak(int n) {
-//         vector<int> dp(n + 1, -1);
-//         return solve(n, dp);
-//     }
-// };
