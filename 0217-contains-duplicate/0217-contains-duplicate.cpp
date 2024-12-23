1abf9659f16cbf2d1154e491577febfe45dbc106
muqeetqazi <141923917+muqeetqazi@users.noreply.github.com>
2024-12-23 14:35:18 +0500

    Time: 22 ms (84.84%), Space: 61.2 MB (75.61%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,13 +1,14 @@
-// Contains Duplicate
-  class Solution {
-  public:
-      bool containsDuplicate(vector<int>& nums) {
-          unordered_map<int,int> mp;
-          for(auto i : nums) mp[i]++;
-          bool flag = false;
-          for(auto i : mp){
-              if(i.second >= 2) return true;
-          }
-          return flag;
-      }
-  };
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
