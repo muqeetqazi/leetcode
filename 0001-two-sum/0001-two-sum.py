94722b55d08cf0920f64048ea7f3336fa489407c
muqeetqazi <141923917+muqeetqazi@users.noreply.github.com>
2024-12-23 15:43:25 +0500

    Time: 2056 ms (36.38%), Space: 13.2 MB (6.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,17 +1,17 @@
-class Solution(object):
+class Solution:
     def twoSum(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
-        ans=[]
+        ans = []  # To store the indices
+
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[i]==target:
+            for j in range(i + 1, len(nums)):  # Inner loop should iterate until the end
+                if nums[i] + nums[j] == target:
                     ans.append(i)
                     ans.append(j)
-                    return ans
+                    return ans  # Return immediately after finding the pair
 
-            
-        return ans
+        return ans  # Return empty list if no solution is found
