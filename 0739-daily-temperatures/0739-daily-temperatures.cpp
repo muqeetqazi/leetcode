447665f591e018a4653db41cb8da34065ac5b277
muqeetqazi <141923917+muqeetqazi@users.noreply.github.com>
2025-01-28 19:56:32 +0500

    Time: 21 ms (65.84%), Space: 102.8 MB (91.3%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
-        vector<int> answer(temperatures.size(), 0);
-        stack<int> stack;
-
-        for (int i = 0; i < temperatures.size(); i++) {
-            while (!stack.empty() && temperatures[i] > temperatures[stack.top()]) {
-                int idx = stack.top();
-                stack.pop();
-                answer[idx] = i - idx;
-            }
-            stack.push(i);
+    vector<int> dailyTemperatures(vector<int>& temperatures)
+    {
+        stack<int>st;
+        int len=temperatures.size();
+        vector<int>ans(len,0);    
+        int i;
+        for(i=len-1;i>=0;i--)
+        {
+            while(st.size()&&temperatures[st.top()]<=temperatures[i])
+                st.pop();
+            if(st.size())
+                ans[i]=st.top()-i;
+            else
+                ans[i]=0;
+            st.push(i);
         }
-
-        return answer;
+        return ans;
     }
 };
